489bdfbe39f5598b6ee91cf4b95e470b47ecafc9
edwarderlick <samirsheik2002@gmail.com>
2026-09-06 01:12:16 +0530

    fix: add on=finalized to emit_transfer to ensure native GEN is withdrawn properly

--- contract/remediate.py
+++ contract/remediate.py
@@ -82,7 +82,7 @@ class RemediateContract(gl.Contract):
 
         # External transfer. If this fails, entire call reverts, restoring self.credits[caller]!
         try:
-            gl.get_contract_at(Address(str(caller))).emit_transfer(value=amount)
+            gl.get_contract_at(Address(str(caller))).emit_transfer(value=amount, on="finalized")
         finally:
             self.withdrawing = False
 
